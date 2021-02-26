d5e9adec74ea4aa0b5bf4ed1b5df423c888ce2f3
Silas de Araujo <silasdearaujo@live.com>
2021-02-26 20:04:34 -0300

    finalizando

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,23 +30,43 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
-    }
+      const products = await AsyncStorage.getItem('@goMarketplace:produtcs');
+
+      if (products){
+        setProducts([...JSON.parse(products)]);
+      };
+    };
 
     loadProducts();
   }, []);
 
   const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+    const productsExists = products.find(p => p.id === product.id);
+
+    if (productsExists) {
+      setProducts(products.map(p => p.id === product.id ? { ...product, quantity: p.quantity + 1 } : p));
+    } else {
+      setProducts([...products, { ...product, quantity: 1 }]);
+    }
+
+    await AsyncStorage.setItem('@goMarketplace:produtcs', JSON.stringify(products));
+  }, [products]);
 
   const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+    const newProducts = products.map(product => product.id === id ? {...product, quantity: product.quantity + 1 } : product);
+
+    setProducts(newProducts);
+
+    await AsyncStorage.setItem('@goMarketplace:produtcs', JSON.stringify(newProducts));
+  }, [products]);
 
   const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+    const newProducts = products.map(product => product.id === id ? {...product, quantity: product.quantity - 1 } : product);
+
+    setProducts(newProducts);
+
+    await AsyncStorage.setItem('@goMarketplace:produtcs', JSON.stringify(newProducts));
+  }, [products]);
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
